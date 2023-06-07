e33e5b8c96a83bb3bc93c884e264d6232a8b33c2
Harshitha <harshith@usc.edu>
2023-06-07 11:33:49 -0700

    Minor updates

--- js/record_status_dashboard.js
+++ js/record_status_dashboard.js
@@ -1,11 +1,11 @@
-CrossProjectPipingModule = {};
+CrossProjectPipingModulePlus = {};
 
-CrossProjectPipingModule.ajax_endpoint = "AJAX_ENDPOINT";
+CrossProjectPipingModulePlus.ajax_endpoint = "AJAX_ENDPOINT";
 
-CrossProjectPipingModule.ajax_complete = function(data, status, xhr) {
+CrossProjectPipingModulePlus.ajax_complete = function(data, status, xhr) {
 	console.log("ajax completed", {data: data, status: status, xhr: xhr});
-	$(".cpp_pipe_all_loader").css('display', 'none');
-	$("button#pipe_all_records").attr('disabled', false);
+	$(".cpp_pipe_all_loader_plus").css('display', 'none');
+	$("button#pipe_all_records_plus").attr('disabled', false);
 	
 	if (status == 'success' && data.responseJSON && data.responseJSON['success'] == true) {
 		window.location.reload();
@@ -18,27 +18,27 @@ CrossProjectPipingModule.ajax_complete = function(data, status, xhr) {
 	}
 }
 
-CrossProjectPipingModule.addButtonAfterJqueryLoaded = function() {
+CrossProjectPipingModulePlus.addButtonAfterJqueryLoaded = function() {
 	if (typeof($) != 'undefined') {
 		// wait 
 		$(function() {
-			$("form#dashboard-config + div").append("<button id='pipe_all_records' class='btn btn-xs btn-rcpurple fs13'><div class='cpp_pipe_all_loader'></div>Sync Records Across Projects</button>");
+			$("form#dashboard-config + div").append("<button id='pipe_all_records_plus' class='btn btn-xs btn-rcpurple fs13'><div class='cpp_pipe_all_loader_plus'></div>Sync Records Across Projects</button>");
 			
-			$("body").on("click", "button#pipe_all_records", function(event) {
+			$("body").on("click", "button#pipe_all_records_plus", function(event) {
 				// show spinning loader icon and disabled pipe button
-				$(".cpp_pipe_all_loader").css('display', 'inline-block');
-				$("button#pipe_all_records").attr('disabled', true);
+				$(".cpp_pipe_all_loader_plus").css('display', 'inline-block');
+				$("button#pipe_all_records_plus").attr('disabled', true);
 				
-				// send ajax request to pipe_all_records endpoint
+				// send ajax request to pipe_all_records_plus endpoint
 				$.get({
-					url: CrossProjectPipingModule.ajax_endpoint,
-					complete: CrossProjectPipingModule.ajax_complete,
+					url: CrossProjectPipingModulePlus.ajax_endpoint,
+					complete: CrossProjectPipingModulePlus.ajax_complete,
 				});
 			});
 		});
 	} else {
-		setTimeout(CrossProjectPipingModule.addButtonAfterJqueryLoaded, 100);
+		setTimeout(CrossProjectPipingModulePlus.addButtonAfterJqueryLoaded, 100);
 	}
 }
 
-CrossProjectPipingModule.addButtonAfterJqueryLoaded();
+CrossProjectPipingModulePlus.addButtonAfterJqueryLoaded();
