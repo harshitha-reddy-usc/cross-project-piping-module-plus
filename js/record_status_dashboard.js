fb2e24f73d0b5aba4396f4d12bdfb1b28ff02e12
Harshitha <harshith@usc.edu>
2023-08-15 13:56:53 -0700

    Batching requests

--- js/record_status_dashboard.js
+++ js/record_status_dashboard.js
@@ -2,15 +2,20 @@ CrossProjectPipingModulePlus = {};
 
 CrossProjectPipingModulePlus.ajax_endpoint = "AJAX_ENDPOINT";
 CrossProjectPipingModulePlus.initial_ajax_endpoint = "INITIAL_AJAX";
+CrossProjectPipingModulePlus.processed_count = 0;
+
 
 CrossProjectPipingModulePlus.ajax_complete = function(data, status, xhr) {
 	console.log("ajax completed", {data: data, status: status, xhr: xhr});
-	$(".cpp_pipe_all_loader_plus").css('display', 'none');
-	$("button#pipe_all_records_plus").attr('disabled', false);
-
 	if (status == 'success' && data.responseJSON && data.responseJSON['success'] == true) {
-		window.location.reload();
+		if (CrossProjectPipingModulePlus.totalRecords === CrossProjectPipingModulePlus.processed_count) {
+			$(".cpp_pipe_all_loader_plus").css('display', 'none');
+			$("button#pipe_all_records_plus").attr('disabled', false);
+			window.location.reload();
+		}
 	} else {
+		$(".cpp_pipe_all_loader_plus").css('display', 'none');
+		$("button#pipe_all_records_plus").attr('disabled', false);
 		if (data.responseJSON && data.responseJSON['error']) {
 			alert(data.responseJSON['error']);
 		} else {
@@ -38,34 +43,31 @@ CrossProjectPipingModulePlus.addButtonAfterJqueryLoaded = function() {
 					}
 				});
 
+
 				var startIndex = 0;
-				var batchSize = 10000;
-				var endIndex = Math.min(startIndex + batchSize, CrossProjectPipingModulePlus.totalRecords);
-				var status = "success";
-				function makeAjaxCall() {
-					if (startIndex <= endIndex && endIndex <= CrossProjectPipingModulePlus.totalRecords && status === "success") {
-						// send ajax request to pipe_all_records_plus endpoint;
-						$.get({
-							url: CrossProjectPipingModulePlus.ajax_endpoint,
-							//async: false,
-							data: {
-								start_index: startIndex,
-								end_index: endIndex
-							},
-							success: function(data) {
-								startIndex = endIndex + 1;
-								endIndex = Math.min(startIndex + batchSize, CrossProjectPipingModulePlus.totalRecords);
-								makeAjaxCall(); // Recursive call to the function
-							},
-							error: function(jqXHR, textStatus, errorThrown) {
-								console.log("Error:", errorThrown);
-								status = "failure";
-							},
-							complete: CrossProjectPipingModulePlus.ajax_complete
-						});
-					}
+				const totalItems = CrossProjectPipingModulePlus.totalRecords;
+				const batchSize = 10000;
+				let remainingItems = totalItems;
+
+				while (remainingItems > 0) {
+					const currentBatchSize = Math.min(batchSize, remainingItems);
+					CrossProjectPipingModulePlus.processed_count += currentBatchSize;
+					endIndex = startIndex + currentBatchSize;
+
+					$.get({
+						url: CrossProjectPipingModulePlus.ajax_endpoint,
+						async: false,
+						data: {
+							start_index: startIndex,
+							end_index: endIndex
+						},
+						complete: CrossProjectPipingModulePlus.ajax_complete
+					});
+
+					remainingItems -= currentBatchSize;
+					startIndex = endIndex + 1;
 				}
-				makeAjaxCall();
+				console.log("All items processed!");
 			});
 		});
 	} else {
